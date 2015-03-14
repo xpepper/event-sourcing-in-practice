4184d438e4f80df766958ecefaff9b10db0461b3
Pietro Di Bello <pietro.dibello@xpeppers.com>
2015-03-14 23:54:09 +0100

    Fixing the github account

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Slide content is subject to change as we keep on learning :).
 
 ## Pre-built slides
 
-Slides are automatically built and published using Travis CI. You may find the latest version in the `gh-pages` branch or at [the associated Github Page for this repository](http://ookami86.github.io/event-sourcing-in-practice/index.html)
+Slides are automatically built and published using Travis CI. You may find the latest version in the `gh-pages` branch or at [the associated Github Page for this repository](http://xpepper.github.io/event-sourcing-in-practice/index.html)
 
-[![Build Status](https://travis-ci.org/Ookami86/event-sourcing-in-practice.png?branch=master)](https://travis-ci.org/Ookami86/event-sourcing-in-practice)
+[![Build Status](https://travis-ci.org/xpepper/event-sourcing-in-practice.png?branch=master)](https://travis-ci.org/xpepper/event-sourcing-in-practice)
 
 ## Building the slides from scratch
 
--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,7 +1,7 @@
 # Take from and modified http://sleepycoders.blogspot.de/2013/03/sharing-travis-ci-generated-files.html?_escaped_fragment_#!
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
-  
+
   TEMP_DIR=/tmp/deploy
   #copy data we're interested in to other place
   mkdir $TEMP_DIR
@@ -13,7 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Ookami86/event-sourcing-in-practice.git  gh-pages
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/xpepper/event-sourcing-in-practice.git  gh-pages
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
@@ -22,7 +22,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages 
+  git push -fq origin gh-pages
 
   echo -e "Done publishing to gh-pages.\n"
 fi
